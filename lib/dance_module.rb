63584997c257bd763728988a0d8610ea6be05e66
Laura Felce <crepu83@yahoo.it>
2018-12-18 08:56:31 +0000

    Automatically backed up by Learn

--- lib/dance_module.rb
+++ lib/dance_module.rb
@@ -6,7 +6,7 @@ module Dance
 
   def jump
     "Look how high I'm jumping!"
-  end  
+  end
 
   def pirouette
    "I'm doing a pirouette"
@@ -15,5 +15,5 @@ module Dance
  def take_a_bow
    "Thank you, thank you. It was a pleasure to dance for you all."
  end
-      
+
 end  
